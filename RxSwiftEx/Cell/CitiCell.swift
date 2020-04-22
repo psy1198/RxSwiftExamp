8cec10e1d4383cbc4a23dc2aa8f68fc5624ba569
SeongNamKim <>
2020-04-22 22:37:51 +0900

    First Commit

---
+++ RxSwiftEx/Cell/CitiCell.swift
@@ -0,0 +1,25 @@
+//
+//  CitiCell.swift
+//  RxSwiftEx
+//
+//  Created by 김성남 on 2020/04/15.
+//  Copyright © 2020 김성남. All rights reserved.
+//
+
+import UIKit
+
+class CitiCell: UITableViewCell {
+    @IBOutlet weak var citiLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
